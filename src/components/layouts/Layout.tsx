992a315cb1f001849de62d9c4fa29a6649fb4c32
itaminul <itaminul@gmail.com>
2024-01-20 23:55:26 +0600

    chat box modify

--- src/components/layouts/Layout.tsx
+++ src/components/layouts/Layout.tsx
@@ -1,15 +1,16 @@
-import { ReactNode } from "react";
+import { ReactNode, useState } from "react";
 import AdminSidebar from "./AdminSidebar";
 import { RootState } from "../../redux/store";
 
 interface props {
   children?: ReactNode;
 }
-import { Container, Grid } from "@mui/material";
+import { Container, Fab, Grid } from "@mui/material";
 import { useSelector } from "react-redux";
 import ChatBoard from "../liveChat/ChatBoard";
 import MessageBox from "../liveChat/MessageBox";
-
+import Chatbox from "../liveChat/ChatBoard";
+import ChatIcon from "@mui/icons-material/Chat";
 const Layout = ({ children }: props) => {
   const isDrawerOpen = useSelector((state: RootState) => state.drawer.isOpen)
   const mainContentStyle: React.CSSProperties = {
@@ -20,11 +21,21 @@ const Layout = ({ children }: props) => {
   maxWidth: 1500,
 };
 
+const [isChatboxOpen, setChatboxOpen] = useState(false);
+
+const toggleChatbox = () => {
+  setChatboxOpen(!isChatboxOpen);
+};
+
   return (
     <>
       <AdminSidebar />
       {/* <ChatBoard /> */}
-      <MessageBox />
+      {/* <MessageBox /> */}
+      {isChatboxOpen && <Chatbox onClose={toggleChatbox} />}
+      <Fab className="" color="primary" onClick={toggleChatbox}>
+        <ChatIcon />
+      </Fab>
       <Container component="main" sx={mainContentStyle}>
         <Grid container justifyContent="center">
           <Grid>{children}</Grid>
--- src/components/liveChat/ChatBoard.tsx
+++ src/components/liveChat/ChatBoard.tsx
@@ -6,10 +6,16 @@ import {
   List,
   ListItem,
   ListItemText,
+  Paper,
 } from "@mui/material";
 import './chatBoard.scss'
 
-const Chatbox: React.FC = () => {
+interface ChatboxProps {
+  onClose: () => void;
+}
+
+
+const Chatbox = ({ onClose }: ChatboxProps) => {
   const [message, setMessage] = useState("");
   const [messages, setMessages] = useState<string[]>([]);
 
@@ -21,28 +27,53 @@ const Chatbox: React.FC = () => {
   };
 
   return (
-    <Box className="chatboxContainer">
-      <List className="messagesContainer">
+    <Paper className="chatboxContainer">
+      <div className="chatboxHeader">
+        <div>Chat</div>
+        <Button onClick={onClose}>Close</Button>
+      </div>
+      <div className="chatboxContent">
         {messages.map((msg, index) => (
-          <ListItem key={index}>
-            <ListItemText primary={msg} />
-          </ListItem>
+          <div key={index}>{msg}</div>
         ))}
-      </List>
-      <Box className="inputContainer">
+      </div>
+      <div className="inputContainer">
         <TextField
+          className="inputField"
           variant="outlined"
           size="small"
           label="Type a message"
           value={message}
           onChange={(e) => setMessage(e.target.value)}
-          className="inputField"
         />
         <Button variant="contained" color="primary" onClick={handleSendMessage}>
           Send
         </Button>
-      </Box>
-    </Box>
+      </div>
+    </Paper>
+    
+    // <Box className="chatboxContainer">
+    //   <List className="messagesContainer">
+    //     {messages.map((msg, index) => (
+    //       <ListItem key={index}>
+    //         <ListItemText primary={msg} />
+    //       </ListItem>
+    //     ))}
+    //   </List>
+    //   <Box className="inputContainer">
+    //     <TextField
+    //       variant="outlined"
+    //       size="small"
+    //       label="Type a message"
+    //       value={message}
+    //       onChange={(e) => setMessage(e.target.value)}
+    //       className="inputField"
+    //     />
+    //     <Button variant="contained" color="primary" onClick={handleSendMessage}>
+    //       Send
+    //     </Button>
+    //   </Box>
+    // </Box>
   );
 };
 
