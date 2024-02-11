cd488edbcc836801d4dec536f79bb795aa071649
itaminul <itaminul@gmail.com>
2024-02-12 00:02:42 +0600

    vendor setup submit

--- src/components/setup/vendor-setup/CreaetVendorModal.tsx
+++ src/components/setup/vendor-setup/CreaetVendorModal.tsx
@@ -1,18 +1,17 @@
 import Modal from "@mui/material/Modal";
-import { Autocomplete, Box, Button, Grid, TextField, Select, MenuItem } from "@mui/material";
-import { useFormik } from 'formik';
+import { Box, Button, Grid, TextField, Select, MenuItem } from "@mui/material";
+import { ErrorMessage, useFormik } from 'formik';
 import { VendorDataItems } from "./VendorDataTypes";
 import { CreateVendorValidation } from "./CreateVendorValidation";
-import { useGetVendoerSetupQuery } from "../../../redux/services/vendoerSetupAPI"; 
-import { number, string } from "yup";
+import { useCreateVendorSetupMutation, useGetVendoerSetupQuery } from "../../../redux/services/vendoerSetupAPI"; 
 import { useGetVendorCountrySetupDataQuery } from "../../../redux/services/vendorCountrySetupAPI";
+import { useState } from "react";
 interface ModalProps {
   open: boolean;
   onClose: () => void
 }
 const initialValues = {
-  name: "",
-  id: 0,
+  vendorName: null,
   vendorType: null,
   vendorCountryType: null,
   vendorCountry: null,
@@ -33,27 +32,34 @@ const options: Option[] = [
   { label: 'Option 3', value: 'option3' },
 ];
 
-
-
-const top100Films = [
-  { title: "The Shawshank Redemption", year: 1994 },
-  { title: "The Godfather", year: 1972 },
-  { title: "The Godfather: Part II", year: 1974 },
-  { title: "The Dark Knight", year: 2008 },
-  { title: "12 Angry Men", year: 1957 },
-  { title: "Schindler's List", year: 1993 },
-  { title: "Pulp Fiction", year: 1994 },
-];
 const CreateVendorModal = ({ open, onClose }: ModalProps) => {
+    const [successOpen, setSuccessOpen] = useState(false);
   const { data } = useGetVendoerSetupQuery();
   const { data: vendorCountry } = useGetVendorCountrySetupDataQuery();
+  const [ saveVendor ] = useCreateVendorSetupMutation();
   console.log("get data vendorCountry", vendorCountry);
   const { values, handleBlur, handleChange, handleSubmit, errors } =
     useFormik<VendorDataItems>({
       initialValues:initialValues,
       validationSchema: CreateVendorValidation,
-      onSubmit: (values: any) => {
-        console.log("values", values);
+      onSubmit: async(values: VendorDataItems) => {
+       try {
+         const fomateData = {
+           vendorName: values.vendorName,
+           vendoerDescription: values.vendoerDescription,
+           vendorType: values.vendorType,
+           vendorCountryType: values.vendorCountryType,
+           vendorCountry: values.vendorCountry,
+           vendorOfficeName: values.vendorOfficeName,
+           vendorOfficeLocation: values.vendorOfficeLocation,
+           orgId: Number(values.orgId),
+         };
+         await saveVendor(fomateData);
+         setSuccessOpen(true);
+         console.log("values", values);
+       } catch (error) {
+        console.log("error", ErrorMessage)
+       }
       },
     });
   return (
@@ -75,17 +81,19 @@ const CreateVendorModal = ({ open, onClose }: ModalProps) => {
             <Grid container spacing={1}>
               <Grid item xs={12} sm={4}>
                 <TextField
-                  name="name"
-                  id="name"
+                  name="vendorName"
+                  id="vendorName"
                   fullWidth
                   label="Vendor Name"
                   variant="outlined"
                   onChange={handleChange}
                   onBlur={handleBlur}
-                  value={values.name}
+                  value={values.vendorName}
                 />
-                {Touch.name && errors.name && (
-                  <div className="validation-message-color">{errors.name}</div>
+                {Touch.name && errors.vendorName && (
+                  <div className="validation-message-color">
+                    {errors.vendorName}
+                  </div>
                 )}
               </Grid>
               <Grid item xs={12} sm={4}>
@@ -223,23 +231,23 @@ const CreateVendorModal = ({ open, onClose }: ModalProps) => {
                 )}
               </Grid>
               <Grid item xs={12} sm={4}>
-                <Autocomplete
-                  freeSolo
-                  id="free-solo-2-demo"
-                  disableClearable
-                  options={top100Films.map((option) => option.title)}
-                  renderInput={(params) => (
-                    <TextField
-                      {...params}
-                      label="Organization"
-                      InputProps={{
-                        ...params.InputProps,
-                        type: "search",
-                      }}
-                    />
-                  )}
-                />
-              </Grid>
+                <Select
+                  id="orgId"
+                  name="orgId"
+                  sx={{
+                    marginTop: 1,
+                    width: 318,
+                    height: 55,
+                  }}
+                  onChange={handleChange}
+                  onBlur={handleBlur}
+                  value={values.orgId}
+                >                
+                  <MenuItem value={1}>Abc</MenuItem>
+                 
+                </Select>
+                
+                </Grid>
             </Grid>
 
             <Button onClick={onClose}>Cancel</Button>
--- src/components/setup/vendor-setup/VendorDataTypes.ts
+++ src/components/setup/vendor-setup/VendorDataTypes.ts
@@ -1,10 +1,12 @@
 export interface VendorDataItems {
-    vendorType?: number;
-    id?: number;
-    name?: string;
-    vendorCountryType?: number;
-    vendorCountry?: number;
-    vendorOfficeName?: string;
-    vendorOfficeLocation?: string;
-    vendoerPhone?: string
+  vendorType?: number;
+  id?: number;
+  vendorName?: string;
+  vendoerDescription?: string;
+  vendorCountryType?: number;
+  vendorCountry?: number;
+  vendorOfficeName?: string;
+  vendorOfficeLocation?: string;
+  vendoerPhone?: string;
+  orgId?: number;
 }
