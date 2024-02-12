402e313059d79856a3b5c199d99a32e620c07f07
Aminul <aminul@atilimited.net>
2024-02-12 18:53:33 +0600

    vendor edit start

--- src/components/setup/vendor-setup/EditVendorModal.tsx
+++ src/components/setup/vendor-setup/EditVendorModal.tsx
@@ -1,10 +1,11 @@
 // ModalComponent.tsx
 import React, { useEffect, useState } from "react";
 import Modal from "@mui/material/Modal";
-import { Box, Button, Grid, TextField, Typography } from "@mui/material";
+import { Alert, Box, Button, Grid, MenuItem, Select, Snackbar, TextField, Typography } from "@mui/material";
 import { VendorDataItems } from "./VendorDataTypes";
 import { useFormik } from "formik";
 import { CreateVendorValidation } from "./CreateVendorValidation";
+import { useGetVendorCountrySetupDataQuery } from "../../../redux/services/vendorCountrySetupAPI";
 interface EditModalProps {
   open: boolean;
   onClose: () => void;
@@ -34,8 +35,10 @@ const EditVendorModal: React.FC<EditModalProps> = ({
   onClose,
   selectedRowId
 }) => {
-  const [users, setUsers] = useState<User[]>([]);
 
+  const [showSuccessMessage, setShowSuccessMessage] = useState<boolean>(false);
+  const { data: vendorCountry } = useGetVendorCountrySetupDataQuery();  
+  const [users, setUsers] = useState<User[]>([]);
   const [selectedRow, setSelectedRow] = useState<User | any>(null);
   const [fetchValue, setFetchValue] = useState<any>(null);
 
@@ -82,69 +85,199 @@ const EditVendorModal: React.FC<EditModalProps> = ({
       aria-describedby="keep-mounted-modal-description"
     >
       <form onSubmit={handleSubmit}>
-        <Box
-          sx={{
-            "& .MuiTextField-root": { m: 1, width: "35ch" },
-          }}
-          className="modal-size-md"
-        >
-          <Grid container spacing={1}>
-            <Grid item xs={12} sm={4}>
-              <TextField
-                name="name"
-                id="name"
-                fullWidth
-                label="Last Name"
-                variant="outlined"
-                onChange={handleChange}
-                onBlur={handleBlur}
-              //  value={setFetchValue.name}
-              />
-              {Touch.name && errors.name && (
-                <div className="validation-message-color">{errors.name}</div>
-              )}
-            </Grid>
-            <Grid item xs={12} sm={4}>
-              <TextField
-                name="secondname"
-                id="secondname"
-                fullWidth
-                label="Last Name"
-                variant="outlined"
-              />
-            </Grid>
-            <Grid item xs={12} sm={4}>
-              <TextField fullWidth label="Email" variant="outlined" />
-            </Grid>
-          </Grid>
-          <Grid container spacing={1}>
-            <Grid item xs={12} sm={4}>
-              <TextField fullWidth label="Last Name" variant="outlined" />
-            </Grid>
-            <Grid item xs={12} sm={4}>
-              <TextField fullWidth label="Last Name" variant="outlined" />
-            </Grid>
-            <Grid item xs={12} sm={4}>
-              <TextField fullWidth label="Email" variant="outlined" />
-            </Grid>
-          </Grid>
-          <Grid container spacing={1}>
-            <Grid item xs={12} sm={4}>
-              <TextField fullWidth label="Last Name" variant="outlined" />
-            </Grid>
-            <Grid item xs={12} sm={4}>
-              <TextField fullWidth label="Last Name" variant="outlined" />
-            </Grid>
-            <Grid item xs={12} sm={4}>
-              <TextField fullWidth label="Email" variant="outlined" />
-            </Grid>
-          </Grid>
+      <Box
+            sx={{
+              "& .MuiTextField-root": { m: 1, width: "35ch" },
+            }}
+            className="modal-size-md"
+          >
+            <Grid container spacing={1}>
+              <Grid item xs={12} sm={4}>
+                <TextField
+                  name="vendorName"
+                  id="vendorName"
+                  fullWidth
+                  label="Vendor Name"
+                  variant="outlined"
+                  onChange={handleChange}
+                  onBlur={handleBlur}
+                  value={values.vendorName}
+                />
+                {Touch.name && errors.vendorName && (
+                  <div className="validation-message-color">
+                    {errors.vendorName}
+                  </div>
+                )}
+              </Grid>
+              <Grid item xs={12} sm={4}>
+                <TextField
+                  name="vendoerDescription"
+                  id="vendoerDescription"
+                  fullWidth
+                  label="Vendor Description"
+                  variant="outlined"
+                />
+              </Grid>
 
-          <Button onClick={onClose}>Cancel</Button>
-          <Button type="submit" variant="contained">
-            Create
-          </Button>
-        </Box>
+              <Grid item xs={12} sm={4}>
+                <Select
+                  id="vendorType"
+                  name="vendorType"
+                  sx={{
+                    marginTop: 1,
+                    width: 318,
+                    height: 55,
+                  }}
+                  onChange={handleChange}
+                  onBlur={handleBlur}
+                  value={values.vendorType}
+                >
+                  {vendorCountry?.map((vendorCountr) => (
+                    <MenuItem value={1}>{vendorCountr.countryName}</MenuItem>
+                  ))}
+                </Select>
+                {Touch.name && errors.vendorType && (
+                  <div className="validation-message-color">
+                    {errors.vendorType}
+                  </div>
+                )}
+              </Grid>
+            </Grid>
+            <Grid container spacing={1}>
+              <Grid item xs={12} sm={4}>
+                <Select
+                  id="vendorCountryType"
+                  name="vendorCountryType"
+                  sx={{
+                    marginTop: 1,
+                    width: 318,
+                    height: 55,
+                  }}
+                  onChange={handleChange}
+                  onBlur={handleBlur}
+                  value={values.vendorCountryType}
+                >
+                  {vendorCountry?.map((vendorCountr) => (
+                    <MenuItem value={1}>{vendorCountr.countryName}</MenuItem>
+                  ))}
+                </Select>
+                {Touch.name && errors.vendorCountryType && (
+                  <div className="validation-message-color">
+                    {errors.vendorCountryType}
+                  </div>
+                )}
+              </Grid>
+              <Grid item xs={12} sm={4}>
+                <Select
+                  id="vendorCountry"
+                  name="vendorCountry"
+                  sx={{
+                    marginTop: 1,
+                    width: 318,
+                    height: 55,
+                  }}
+                  onChange={handleChange}
+                  onBlur={handleBlur}
+                  value={values.vendorCountry}
+                >
+                  {vendorCountry?.map((vendorCountr) => (
+                    <MenuItem value={1}>{vendorCountr.countryName}</MenuItem>
+                  ))}
+                </Select>
+                {Touch.name && errors.vendorCountry && (
+                  <div className="validation-message-color">
+                    {errors.vendorCountry}
+                  </div>
+                )}
+              </Grid>
+              <Grid item xs={12} sm={4}>
+                <TextField
+                  name="vendorOfficeName"
+                  id="vendorOfficeName"
+                  fullWidth
+                  label="Vendor Office Name"
+                  variant="outlined"
+                  onChange={handleChange}
+                  onBlur={handleBlur}
+                  value={values.vendorOfficeName}
+                />
+                {Touch.name && errors.vendorOfficeName && (
+                  <div className="validation-message-color">
+                    {errors.vendorOfficeName}
+                  </div>
+                )}
+              </Grid>
+            </Grid>
+            <Grid container spacing={1}>
+              <Grid item xs={12} sm={4}>
+                <TextField
+                  name="vendorOfficeLocation"
+                  id="vendorOfficeLocation"
+                  fullWidth
+                  label="Vendor Office Location"
+                  variant="outlined"
+                  onChange={handleChange}
+                  onBlur={handleBlur}
+                  value={values.vendorOfficeLocation}
+                />
+                {Touch.name && errors.vendorOfficeLocation && (
+                  <div className="validation-message-color">
+                    {errors.vendorOfficeLocation}
+                  </div>
+                )}
+              </Grid>
+              <Grid item xs={12} sm={4}>
+                <TextField
+                  name="vendoerPhone"
+                  id="vendoerPhone"
+                  fullWidth
+                  label="Vendor Phone"
+                  variant="outlined"
+                  onChange={handleChange}
+                  onBlur={handleBlur}
+                  value={values.vendoerPhone}
+                />
+                {Touch.name && errors.vendoerPhone && (
+                  <div className="validation-message-color">
+                    {errors.vendoerPhone}
+                  </div>
+                )}
+              </Grid>
+              <Grid item xs={12} sm={4}>
+                <Select
+                  id="orgId"
+                  name="orgId"
+                  sx={{
+                    marginTop: 1,
+                    width: 318,
+                    height: 55,
+                  }}
+                  onChange={handleChange}
+                  onBlur={handleBlur}
+                  value={values.orgId}
+                >                
+                  <MenuItem value={1}>Abc</MenuItem>
+                 
+                </Select>
+                
+                </Grid>
+            </Grid>
+
+            <Snackbar
+            open={showSuccessMessage}
+            autoHideDuration={2000}
+            anchorOrigin={{ vertical: 'top', horizontal: 'center' }}
+          >
+            <Alert severity="success">
+              Data inserted successfully!
+            </Alert>
+          </Snackbar>
+
+            <Button onClick={onClose}>Cancel</Button>
+            <Button type="submit" variant="contained">
+              Create
+            </Button>
+          </Box>
       </form>
     </Modal>
   );
